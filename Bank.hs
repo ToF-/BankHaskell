17e5e3979dd1b3b54255bd0450daf6004d7a9fff
ToF <cthibaut@octo.com>
2017-11-25 16:22:40 +0100

    S print statement; any other command is an error

--- Bank.hs
+++ Bank.hs
@@ -7,22 +7,23 @@ type Credit = Double
 type Debit  = Double
 type Balance= Double
 
-emptyAccount :: Account 
-emptyAccount = []
+empty :: Account 
+empty = []
 
-printStatement :: Monad m => Account -> (String -> m ()) -> m ()
-printStatement acc output = output $ unlines $ header : statement acc 
-    where 
-    statement = map showOperation
-    header = " date | credit | debit | balance " 
-    showOperation (date,c,0,b) = " " ++ date ++ " | " ++ show c ++" | | " ++ show c ++ " " 
-    showOperation (date,0,d,b) = " " ++ date ++ " | | " ++ show d ++" | " ++ show (-d) ++ " " 
+operation :: Monad m => Account -> (m String) -> (String -> m ()) -> m Account
+operation acc inp out = inp >>= \s -> perform (words s) acc out
+    where
+    perform :: Monad m => [String] -> Account -> (String -> m ()) -> m Account
+    perform ["S"] acc out = output acc out >> return acc
+    perform s _ out       = out ((unwords s) ++ "?") >> return acc 
 
-getOperation :: Monad m => Account -> (m String) -> m Account
-getOperation acc input = input >>= \s -> perform (words s) acc  
+    output :: Monad m => Account -> (String -> m ()) -> m ()
+    output acc out = out $ unlines $ header : statement acc
 
-perform :: Monad m => [String] -> Account -> m Account
-perform [oper,date,amount] acc = case head oper of
-    'W' -> return ((date, 0, read amount, negate (read amount)) : acc)
-    'D' -> return ((date, read amount, 0, read amount) : acc)
-perform _ acc = return acc
+        where 
+        statement = map showOperation
+        header = " date | credit | debit | balance " 
+        showOperation (date,c,0,b) = " " ++ date ++ " | " ++ show c ++" | | " ++ show c ++ " " 
+        showOperation (date,0,d,b) = " " ++ date ++ " | | " ++ show d ++" | " ++ show (-d) ++ " " 
+
+
--- Specs.hs
+++ Specs.hs
@@ -3,13 +3,29 @@ import Control.Monad.Writer
 import Bank
 
 main = hspec $ do
+    describe "operation" $ do
+        describe "S prints the account statement" $ do
+            it "when empty account, prints just the header" $ do
+                let out = \s -> writer ((), s)
+                    inp = return "S"
+                    run = operation empty inp out 
+                lines (snd (runWriter run)) `shouldBe` [" date | credit | debit | balance "] 
+
+        describe "prints an error message" $ do
+            it "when no command or unknown command" $ do
+                let out = \s -> writer ((), s)
+                    inp = return "Foo Bar"
+                    run = operation empty inp out
+                lines (snd (runWriter run)) `shouldBe` ["Foo Bar?"]
+
+{--
     describe "printStatement" $ do
         describe "should output the account statement" $ do
             it "for an empty account" $ do
                 let acc = emptyAccount
                     out = \s -> writer ((), s) 
                     w    = printStatement acc out
-                snd (runWriter w) `shouldBe` " date | credit | debit | balance \n"
+                snd (runWriter w) `shouldBe` 
 
     describe "getOperation" $ do
         describe "should get an operation and perform it on the account" $ do
@@ -27,7 +43,7 @@ main = hspec $ do
                 lines (snd (runWriter w)) `shouldBe` 
                         [" date | credit | debit | balance "
                         ," 2017/25/01 | | 48.07 | -48.07 "]
-
+--}
 
                         
                 
